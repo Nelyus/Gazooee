2581189789abb28ba95dea060b1e6ca144c6b834
Pierre Monod-Broca <pierre@monod-broca.fr>
2016-07-05 18:11:22 +0200

    better tests

--- Tests/Gazooee/GazooeeTests.swift
+++ Tests/Gazooee/GazooeeTests.swift
@@ -3,7 +3,15 @@ import XCTest
 @testable import GazooeeConfig
 //import func Gazooee.log
 
+class MockConsole: Destination {
+    var records: [(Record, String)] = []
+    func log(record: Record, value: @noescape () -> (Any)) {
+        records.append((record, "\(value())"))
+    }
+}
+
 class GazooeeTests: XCTestCase {
+    var defaultConsole = MockConsole()
     func testDefaultLogs() {
         log(.debug, "this is debug")
         log(.info, "this is info")
@@ -11,39 +19,45 @@ class GazooeeTests: XCTestCase {
         log(.error, "this is error")
     }
 
-    func testNoLog() {
-        masterDestination = Console().filtered({_ in false})
+    func testNoFilter() {
+        masterDestination = defaultConsole
         log(.debug, "this is debug")
         log(.info, "this is info")
         log(.warn, "this is warn")
         log(.error, "this is error")
+        XCTAssertEqual(4, defaultConsole.records.count)
+    }
+
+    func testNoLog() {
+        masterDestination = Filter({_ in false}, destination: defaultConsole)
+        log(.debug, "this is debug")
+        log(.info, "this is info")
+        log(.warn, "this is warn")
+        log(.error, "this is error")
+        XCTAssertEqual(0, defaultConsole.records.count)
     }
 
     func testWarnAndErrorLogs() {
-        masterDestination = Console().filtered(above: .warn)
+        masterDestination = Filter(above: .warn, destination: defaultConsole)
         log(.debug, "this is debug")
         log(.info, "this is info")
         log(.warn, "this is warn")
         log(.error, "this is error")
+        XCTAssertEqual(2, defaultConsole.records.count)
     }
 
-    func testOtherSyntax() {
-        masterDestination = Filter(above: .warn, destination: Console())
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
-    }
-
-    func testMultiSyntax() {
+    func testMulti() {
+        let errorConsole = MockConsole()
         masterDestination = Multi([
-            Filter({ $0.level <= .warn }, destination: Console()),
-            Filter({ $0.level >= .warn }, destination: ConsoleNSLog()),
+            Filter({ $0.level <= .warn }, destination: defaultConsole),
+            Filter({ $0.level >= .warn }, destination: errorConsole),
         ])
         log(.debug, "this is debug")
         log(.info, "this is info")
         log(.warn, "this is warn")
         log(.error, "this is error")
+        XCTAssertEqual(3, defaultConsole.records.count)
+        XCTAssertEqual(2, errorConsole.records.count)
     }
 
     static var allTests : [(String, (GazooeeTests) -> () throws -> Void)] {
