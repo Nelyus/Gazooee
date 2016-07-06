9c369b02889d1fcf9bcdbb927ff7e57bb3c9e641
Pierre Monod-Broca <pierre@monod-broca.fr>
2016-07-06 20:51:03 +0200

    made public Filter

--- Sources/GazooeeConfig/Filter.swift
+++ Sources/GazooeeConfig/Filter.swift
@@ -8,28 +8,28 @@
 
 import Foundation
 
-struct Filter : Destination {
+public struct Filter : Destination {
     let predicate: (Record) -> (Bool)
     let destination: Destination
 
-    init(_ predicate: (Record) -> (Bool), destination: Destination) {
+    public init(_ predicate: (Record) -> (Bool), destination: Destination) {
         self.predicate = predicate
         self.destination = destination
     }
 
-    init(above minLevel: Level, destination: Destination) {
+    public init(above minLevel: Level, destination: Destination) {
         self.init({ $0.level >= minLevel }, destination: destination)
     }
 
     #if swift(>=3.0)
-    func log(record: Record, value: @noescape () -> (Any)) {
+    public func log(record: Record, value: @noescape () -> (Any)) {
         guard predicate(record) else {
             return
         }
         destination.log(record: record, value: value)
     }
     #else
-    func log(record record: Record, @noescape value: () -> (Any)) {
+    public func log(record record: Record, @noescape value: () -> (Any)) {
         guard predicate(record) else {
             return
         }
