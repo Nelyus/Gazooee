602bcd8b2901a3c358c26bf02be34d5fe3738576
Pierre Monod-Broca <pierre@monod-broca.fr>
2016-07-11 11:31:12 +0200

    update tests

--- Tests/Gazooee/GazooeeTests.swift
+++ Tests/Gazooee/GazooeeTests.swift
@@ -5,63 +5,71 @@ import XCTest
 
 class MockConsole: Destination {
     var records: [(Record, String)] = []
+    #if swift(>=3.0)
     func log(record: Record, value: @noescape () -> (Any)) {
         records.append((record, "\(value())"))
     }
+    #else
+    func log(record record: Record, @noescape value: () -> (Any)) {
+        records.append((record, "\(value())"))
+    }
+    #endif
 }
 
 class GazooeeTests: XCTestCase {
     var defaultConsole = MockConsole()
+    var logger = Logger(domain: "test")
+
     func testDefaultLogs() {
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
+        logger.log(.debug, "this is debug")
+        logger.log(.info, "this is info")
+        logger.log(.warn, "this is warn")
+        logger.log(.error, "this is error")
     }
 
     // probably not on linux
     func testConsoleNSLog() {
         masterDestination = ConsoleNSLog()
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
+        logger.log(.debug, "this is debug")
+        logger.log(.info, "this is info")
+        logger.log(.warn, "this is warn")
+        logger.log(.error, "this is error")
     }
 
     func testNoFilter() {
         masterDestination = defaultConsole
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
+        logger.log(.debug, "this is debug")
+        logger.log(.info, "this is info")
+        logger.log(.warn, "this is warn")
+        logger.log(.error, "this is error")
         XCTAssertEqual(4, defaultConsole.records.count)
     }
 
     func testNoLog() {
         masterDestination = Filter({_ in false}, destination: defaultConsole)
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
+        logger.log(.debug, "this is debug")
+        logger.log(.info, "this is info")
+        logger.log(.warn, "this is warn")
+        logger.log(.error, "this is error")
         XCTAssertEqual(0, defaultConsole.records.count)
     }
 
     func testWarnAndErrorLogs() {
         masterDestination = Filter(above: .warn, destination: defaultConsole)
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
+        logger.log(.debug, "this is debug")
+        logger.log(.info, "this is info")
+        logger.log(.warn, "this is warn")
+        logger.log(.error, "this is error")
         XCTAssertEqual(2, defaultConsole.records.count)
     }
 
     func testMulti() {
         let secondConsole = MockConsole()
         masterDestination = Multi([defaultConsole, secondConsole])
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
+        logger.log(.debug, "this is debug")
+        logger.log(.info, "this is info")
+        logger.log(.warn, "this is warn")
+        logger.log(.error, "this is error")
         XCTAssertEqual(4, defaultConsole.records.count)
         XCTAssertEqual(4, secondConsole.records.count)
     }
@@ -72,10 +80,10 @@ class GazooeeTests: XCTestCase {
             defaultConsole,
             Filter(above: .warn, destination: errorConsole),
         ])
-        log(.debug, "this is debug")
-        log(.info, "this is info")
-        log(.warn, "this is warn")
-        log(.error, "this is error")
+        logger.log(.debug, "this is debug")
+        logger.log(.info, "this is info")
+        logger.log(.warn, "this is warn")
+        logger.log(.error, "this is error")
         XCTAssertEqual(4, defaultConsole.records.count)
         XCTAssertEqual(2, errorConsole.records.count)
     }
