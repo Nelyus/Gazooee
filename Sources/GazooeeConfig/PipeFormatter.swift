b983a0ce465c84507447f3f71b55b384830d8fcc
Pierre Monod-Broca <pierre@monod-broca.fr>
2016-07-15 01:20:05 +0200

    tests - added more, for formatters

---
+++ Sources/GazooeeConfig/PipeFormatter.swift
@@ -0,0 +1,21 @@
+//
+//  PipeFormatter.swift
+//  Gazooee
+//
+//  Created by Pierre on 15/07/2016.
+//
+//
+
+import Foundation
+
+public struct PipeFormatter: Formatter {
+    #if swift(>=3.0)
+    public func format(record: Record, value: @noescape () -> (Any)) -> String {
+        return "| \(record.level) - \(record.domain): \(record.file):\(record.line) - \(record.function)\n| \(value())"
+    }
+    #else
+    public func format(record record: Record, @noescape value: () -> (Any)) -> String {
+        return "| \(record.level) - \(record.domain): \(record.file):\(record.line) - \(record.function)\n| \(value())"
+    }
+    #endif
+}
--- Tests/Gazooee/PipeFormatterTests.swift
+++ Tests/Gazooee/PipeFormatterTests.swift
@@ -1,20 +1,31 @@
 //
-//  OneLineFormatterTests.swift
+//  PipeFormatterTests.swift
 //  Gazooee
 //
-//  Created by Pierre on 14/07/2016.
+//  Created by Pierre on 15/07/2016.
 //
 //
 
-import Foundation
 import XCTest
 @testable import GazooeeConfig
 
-class OneLineFormatterTests: XCTestCase {
 
-    func testOneLineFormatter() {
-        let sut = OneLineFormatter()
+class PipeFormatterTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testPipeFormatter() {
+        let sut = PipeFormatter()
         let result = sut.format(record: Record(level: .debug, domain: "testsDomain", file: "file.swift", line: 42, function: "testFunction"), value: {"TheValue"})
+        print(result)
         XCTAssertTrue(result.contains("debug"))
         XCTAssertTrue(result.contains("testsDomain"))
         XCTAssertTrue(result.contains("file.swift"))
