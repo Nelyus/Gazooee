40e9295abc6f785d621982b85e08b5cfcd261286
Pierre Monod-Broca <pierre@monod-broca.fr>
2016-07-15 00:30:12 +0200

    OneLineFormatter - adds a test, and adds the domain

---
+++ Tests/Gazooee/OneLineFormatterTests.swift
@@ -0,0 +1,25 @@
+//
+//  OneLineFormatterTests.swift
+//  Gazooee
+//
+//  Created by Pierre on 14/07/2016.
+//
+//
+
+import Foundation
+import XCTest
+@testable import GazooeeConfig
+
+class OneLineFormatterTests: XCTestCase {
+
+    func testOne() {
+        let sut = OneLineFormatter()
+        let result = sut.format(record: Record(level: .debug, domain: "testsDomain", file: "file.swift", line: 42, function: "testFunction"), value: {"TheValue"})
+        XCTAssertTrue(result.contains("debug"))
+        XCTAssertTrue(result.contains("testsDomain"))
+        XCTAssertTrue(result.contains("file.swift"))
+        XCTAssertTrue(result.contains("42"))
+        XCTAssertTrue(result.contains("testFunction"))
+        XCTAssertTrue(result.contains("TheValue"))
+    }
+}
