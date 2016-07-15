45b405d6ac0fbe41dd102e0b237f0b3369c2fcd8
Pierre Monod-Broca <pierre@monod-broca.fr>
2016-07-15 15:12:26 +0200

    Logger - add a method by log level
    adds Logger.debug, logger.info, logger.warn, logger.error

--- Sources/Gazooee/Log.swift
+++ Sources/Gazooee/Log.swift
@@ -85,7 +85,61 @@ public struct Logger {
     public func debug(@autoclosure _ value: () -> (Any), _file file: String = #file, _line line: Int = #line, _function function: String = #function) {
         var valueCache: Any? = nil
         masterDestination.log(
-            record: Record(level: level, subsystem: subsystem, file: file, line: line, function: function),
+            record: Record(level: .debug, subsystem: subsystem, file: file, line: line, function: function),
+            value: { valueCache ??= value() }
+        )
+    }
+    #endif
+
+    #if swift(>=3.0)
+    public func info(_ value: @autoclosure () -> (Any), _file file: String = #file, _line line: Int = #line, _function function: String = #function) {
+        var valueCache: Any? = nil
+        masterDestination.log(
+            record: Record(level: .info, subsystem: subsystem, file: file, line: line, function: function),
+            value: { valueCache ??= value() }
+        )
+    }
+    #else
+    public func info(@autoclosure _ value: () -> (Any), _file file: String = #file, _line line: Int = #line, _function function: String = #function) {
+        var valueCache: Any? = nil
+        masterDestination.log(
+            record: Record(level: .info, subsystem: subsystem, file: file, line: line, function: function),
+            value: { valueCache ??= value() }
+        )
+    }
+    #endif
+
+    #if swift(>=3.0)
+    public func warn(_ value: @autoclosure () -> (Any), _file file: String = #file, _line line: Int = #line, _function function: String = #function) {
+        var valueCache: Any? = nil
+        masterDestination.log(
+            record: Record(level: .warn, subsystem: subsystem, file: file, line: line, function: function),
+            value: { valueCache ??= value() }
+        )
+    }
+    #else
+    public func warn(@autoclosure _ value: () -> (Any), _file file: String = #file, _line line: Int = #line, _function function: String = #function) {
+        var valueCache: Any? = nil
+        masterDestination.log(
+            record: Record(level: .warn, subsystem: subsystem, file: file, line: line, function: function),
+            value: { valueCache ??= value() }
+        )
+    }
+    #endif
+
+    #if swift(>=3.0)
+    public func error(_ value: @autoclosure () -> (Any), _file file: String = #file, _line line: Int = #line, _function function: String = #function) {
+        var valueCache: Any? = nil
+        masterDestination.log(
+            record: Record(level: .error, subsystem: subsystem, file: file, line: line, function: function),
+            value: { valueCache ??= value() }
+        )
+    }
+    #else
+    public func error(@autoclosure _ value: () -> (Any), _file file: String = #file, _line line: Int = #line, _function function: String = #function) {
+        var valueCache: Any? = nil
+        masterDestination.log(
+            record: Record(level: .error, subsystem: subsystem, file: file, line: line, function: function),
             value: { valueCache ??= value() }
         )
     }
---
+++ Tests/Gazooee/LoggerTests.swift
@@ -0,0 +1,105 @@
+//
+//  LoggerTests.swift
+//  Gazooee
+//
+//  Created by Pierre on 15/07/2016.
+//
+//
+
+import XCTest
+@testable import Gazooee
+@testable import GazooeeConfig
+
+class LoggerTests: XCTestCase {
+    let buffer = RecordBuffer()
+    let sut = Logger(subsystem: "tests-subsystem")
+
+    override func setUp() {
+        masterDestination = buffer
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testLogDebug() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.log(.debug, "TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.debug, result.record.level)
+    }
+
+    func testDebug() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.debug("TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.debug, result.record.level)
+    }
+
+    func testLogInfo() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.log(.info, "TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.info, result.record.level)
+    }
+
+    func testInfo() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.info("TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.info, result.record.level)
+    }
+
+    func testLogWarn() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.log(.warn, "TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.warn, result.record.level)
+    }
+
+    func testWarn() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.warn("TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.warn, result.record.level)
+    }
+
+    func testLogError() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.log(.error, "TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.error, result.record.level)
+    }
+
+    func testError() {
+        let sut = Logger(subsystem: "tests")
+        //
+        sut.error("TheValue")
+        XCTAssertEqual(1, buffer.records.count)
+        let result = buffer.records[0]
+        XCTAssertEqual("TheValue", result.value as! String)
+        XCTAssertEqual(.error, result.record.level)
+    }
+}
