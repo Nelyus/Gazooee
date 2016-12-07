0c60643f2c1d89bd222ccdfa0020676c84b12b86
Pierre Monod-Broca <pierre@monod-broca.fr>
2016-12-07 22:32:18 +0100

    added sample targets

--- Package.swift
+++ Package.swift
@@ -9,6 +9,20 @@ let package = Package(
         ),
         Target(
             name: "GazooeeConfig"
+        ),
+        Target(
+            name: "GazooeeSampleApp",
+            dependencies: [
+                .Target(name: "Gazooee"),
+                .Target(name: "GazooeeConfig"),
+                .Target(name: "GazooeeSampleLib"),
+            ]
+        ),
+        Target(
+            name: "GazooeeSampleLib",
+            dependencies: [
+                .Target(name: "Gazooee"),
+            ]
         )
     ]
 )
